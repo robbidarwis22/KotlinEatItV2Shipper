eff840a3a2ee535ba174c4654932a88abeb95c8f
robbidarwis22 <robbidarwis22@gmail.com>
2020-06-01 15:19:34 +0700

    Authentication shipper

---
+++ app/src/main/java/com/example/kotlineatitv2shipper/model/ShipperUserModel.kt
@@ -0,0 +1,8 @@
+package com.example.kotlineatitv2shipper.model
+
+class ShipperUserModel {
+    var uid:String? = null
+    var name:String? = null
+    var phone:String? = null
+    var isActive:Boolean = false
+}
